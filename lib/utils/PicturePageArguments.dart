cceb30e75bbfc693a8eb4b373fdc45bd504fd05c
Meir Hemed <38756487+hemed1@users.noreply.github.com>
2025-12-18 23:23:17 +0200

    4.0.1-(1)

--- lib/utils/PicturePageArguments.dart
+++ lib/utils/PicturePageArguments.dart
@@ -1,12 +1,13 @@
 
-import '../models/movie.dart';
+import '../models/Movie.dart';
 
 
 class PicturePageArguments
 {
+  final Movie         movie;
   final List<String>  moviesLinks;
   final String        movieTitle;
 
-  PicturePageArguments({required this.movieTitle, required this.moviesLinks});
+  PicturePageArguments({required this.movieTitle, required this.moviesLinks, required this.movie});
 
 }
